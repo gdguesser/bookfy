de761df5f998b38139017416a04dbf29f9e76d0d
Gabriel Guesser <gdguesser@gmail.com>
2022-04-12 22:11:07 +0100

    First commit, already did a few things, but still a long way to go

---
+++ cmd/api/handlers.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+}
+
+func (app *application) Login(w http.ResponseWriter, r *http.Request) {
+	type credentials struct {
+		UserName string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	var creds credentials
+	var payload jsonResponse
+
+	err := app.readJSON(w, r, &creds)
+	if err != nil {
+		app.errorLog.Println(err)
+		payload.Error = true
+		payload.Message = "Invalid json supplied or json missing entirely"
+		_ = app.writeJSON(w, http.StatusBadRequest, payload)
+	}
+
+	//TODO authenticate
+	app.infoLog.Println(creds.UserName, creds.Password)
+
+	//send back a response
+	payload.Error = false
+	payload.Message = "Signed in"
+
+	err = app.writeJSON(w, http.StatusOK, payload)
+	if err != nil {
+		app.errorLog.Println(err)
+	}
+}
