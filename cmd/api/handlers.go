09a09a9b8d110a4546f32476957262f981305445
Gabriel Guesser <gdguesser@gmail.com>
2022-04-25 09:35:20 +0100

    creating the route and handler to update/inser user

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -115,5 +115,54 @@ func (app *application) AllUsers(w http.ResponseWriter, r *http.Request) {
 		Data:    envelope{"users": all},
 	}
 
-	app.writeJSON(w, http.StatusOK, payload) 
+	app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *application) EditUser(w http.ResponseWriter, r *http.Request) {
+	var user data.User
+	err := app.readJSON(w, r, user)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	if user.ID == 0 {
+		//add user
+		if _, err := app.models.User.Insert(user); err != nil {
+			app.errorJSON(w, err)
+			return
+		}
+	} else {
+		//edit user
+		u, err := app.models.User.GetById(user.ID)
+		if err != nil {
+			app.errorJSON(w, err)
+			return
+		}
+
+		u.Email = user.Email
+		u.FirstName = user.FirstName
+		u.LastName = user.LastName
+
+		if err := u.Update(); err != nil {
+			app.errorJSON(w, err)
+			return
+		}
+
+		//if password != empty string, update password
+		if user.Password != "" {
+			err := u.ResetPassword(user.Password)
+			if err != nil {
+				app.errorJSON(w, err)
+				return
+			}
+		}
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Changes saved",
+	}
+
+	_ = app.writeJSON(w, http.StatusAccepted, payload)
 }
